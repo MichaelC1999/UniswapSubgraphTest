d4d4b5a8e451e3884d2ba27c0229c79f18a7ea5a
Michael <michaelcarroll1999@gmail.com>
2022-03-08 12:18:17 -0500

    Reformatted, linted, documented

--- src/mapping.ts
+++ src/mapping.ts
@@ -8,115 +8,98 @@ import {
   Swap as SwapEvent
 } from '../generated/templates/Pool/Pool'
 
-function newFactory(): Factory {
-  let factory = new Factory("SingleFactory");
-  factory.uniqueUserCount = new BigInt(0);
-  return factory;
-}
+// FACTORY FUNCTIONS
+// These functions initiate/create the one Factory identity to hold the unique user count
 
-export function handleSetOwner(owner: Address): void {
+function initFactory(): Factory {
+  //This function loads the factory for all event handlers, and calls the newFactory() function if there is no Factory instance yet
   let factory = Factory.load("SingleFactory");
   if (!factory) {
     factory = newFactory();
   }
+  return factory;
+}
 
+function newFactory(): Factory {
+  // This function creates the Factory instance if it has not been initiated yet
+  const factory = new Factory("SingleFactory");
+  factory.uniqueUserCount = new BigInt(0);
+  return factory;
+}
+
+// UNIQUE USER ADDRESS CHECK FUNCTIONS
+// These functions check the given address parameter as the ID of a UserObj instance
+// If the address is not used, create a new UserObj instance
+
+function checkSenderAddr(sender: Address, factory: Factory): void {
+  //This function checks for an instance of the UserObj entity matching the sender address parameter
+  let user = UserObj.load(sender.toHex());
+  if (!user) {
+    // If the sender param address has not been used to instantiate a UserObj, create a new UserObj instance
+    user = new UserObj(sender.toHex());
+    user.save();
+    //Increment the uniqueUserCount on the factory instance
+    const num = +(1) + +(factory.uniqueUserCount.toI32());
+    factory.uniqueUserCount = new BigInt(num);
+    factory.save();
+  }
+}
+
+function checkRecipientAddr(recipient: Address, factory: Factory): void {
+  //This function checks for an instance of the UserObj entity matching the recipient address parameter
+  let user = UserObj.load(recipient.toHex());
+  if (!user) {
+    // If the recipient param address has not been used to instantiate a UserObj, create a new UserObj instance
+    user = new UserObj(recipient.toHex());
+    user.save();
+    //Increment the uniqueUserCount on the factory instance
+    const num = +(1) + +(factory.uniqueUserCount.toI32());
+    factory.uniqueUserCount = new BigInt(num);
+    factory.save();
+  }
+}
+
+export function checkOwnerAddr(owner: Address, factory: Factory): void {
+  //This function checks for an instance of the UserObj entity matching the owner address parameter
   let user = UserObj.load(owner.toHex());
   if (!user) {
-    user = new UserObj(owner.toHex())
-    let num = +(1) + +(factory.uniqueUserCount.toI32())
-    factory.uniqueUserCount = new BigInt(num);
-    factory.save()
-  }
-}
-
-export function handleBurnCheckUniqueAddr(event: BurnEvent ): void {
-  let factory = Factory.load("SingleFactory");
-  if (!factory) {
-    factory = newFactory()
-  }
-    
-  let user = UserObj.load(event.params.owner.toHex());
-  if (!user) {
-    user = new UserObj(event.params.owner.toHex());
-    let num = +(1) + +(factory.uniqueUserCount.toI32())
+    // If the owner param address has not been used to instantiate a UserObj, create a new UserObj instance
+    user = new UserObj(owner.toHex());
+    user.save();
+    //Increment the uniqueUserCount on the factory instance
+    const num = +(1) + +(factory.uniqueUserCount.toI32());
     factory.uniqueUserCount = new BigInt(num);
     factory.save();
   }
 }
 
-export function handleFlashCheckUniqueAddr(event: FlashEvent ): void {
-  let factory = Factory.load("SingleFactory");
-  if (!factory) {
-    factory = newFactory()
-  }
-  
-  let startingCount = factory.uniqueUserCount;
+// EVENT HANDLER FUNCTIONS
+// All functions initiate the factory entity, then check the address parameters for an existing UserObj instance
+// The parameters checked in each function are defined in the Pool.ts template for each given event
 
-  let user = UserObj.load(event.params.sender.toHex());
-  if (!user) {
-    user = new UserObj(event.params.sender.toHex());
-    let num = +(1) + +(factory.uniqueUserCount.toI32())
-    factory.uniqueUserCount = new BigInt(num);
-    factory.save();
-  }
-  user = UserObj.load(event.params.recipient.toHex());
-  if (!user) {
-    user = new UserObj(event.params.recipient.toHex());
-    let num = +(1) + +(factory.uniqueUserCount.toI32());
-    factory.uniqueUserCount = new BigInt(num);
-    factory.save();
-  }
-
-  if (factory.uniqueUserCount !== startingCount) factory.save()
+export function handleCheckUniqueAddrBurn(event: BurnEvent ): void {
+  const factory = initFactory();
+  // Pool.ts template defines the owner parameter in Burn Events
+  checkOwnerAddr(event.params.owner, factory);
 }
 
-export function handleMintCheckUniqueAddr(event: MintEvent ): void {
-  let factory = Factory.load("SingleFactory");
-  if (!factory) {
-    factory = newFactory()
-  }
-  
-  let startingCount = factory.uniqueUserCount;
-  let user = UserObj.load(event.params.owner.toHex());
-  if (!user) {
-    user = new UserObj(event.params.owner.toHex());
-    let num = +(1) + +(factory.uniqueUserCount.toI32())
-    factory.uniqueUserCount = new BigInt(num);
-    factory.save();
-  }
-  user = UserObj.load(event.params.sender.toHex());
-  if (!user) {
-    user = new UserObj(event.params.sender.toHex());
-    let num = +(1) + +(factory.uniqueUserCount.toI32())
-    factory.uniqueUserCount = new BigInt(num);
-    factory.save();
-  }
-
-  if (factory.uniqueUserCount !== startingCount) factory.save()
+export function handleCheckUniqueAddrFlash(event: FlashEvent ): void {
+  const factory = initFactory();
+  // Pool.ts template defines the sender parameter and the recipient parameter for Flash Events
+  checkSenderAddr(event.params.sender, factory);
+  checkRecipientAddr(event.params.recipient, factory);
 }
 
-export function handleSwapCheckUniqueAddr(event: SwapEvent ): void {
-  let factory = Factory.load("SingleFactory");
-  if (!factory) {
-    factory = newFactory()
-  }
-  
-  let startingCount = factory.uniqueUserCount;
+export function handleCheckUniqueAddrMint(event: MintEvent ): void {
+  const factory = initFactory();
+  // Pool.ts template defines the owner parameter and the sender parameter for Mint Events
+  checkOwnerAddr(event.params.owner, factory);
+  checkSenderAddr(event.params.sender, factory);
+}
 
-  let user = UserObj.load(event.params.sender.toHex());
-  if (!user) {
-    user = new UserObj(event.params.sender.toHex());
-    let num = +(1) + +(factory.uniqueUserCount.toI32())
-    factory.uniqueUserCount = new BigInt(num);
-    factory.save();
-  }
-  user = UserObj.load(event.params.recipient.toHex());
-  if (!user) {
-    user = new UserObj(event.params.recipient.toHex());
-    let num = +(1) + +(factory.uniqueUserCount.toI32())
-    factory.uniqueUserCount = new BigInt(num);
-    factory.save();
-  }
-
-  if (factory.uniqueUserCount !== startingCount) factory.save()
+export function handleCheckUniqueAddrSwap(event: SwapEvent ): void {
+  const factory = initFactory();
+  // Pool.ts template defines the sender parameter and the recipient parameter for Swap Events
+  checkSenderAddr(event.params.sender, factory);
+  checkRecipientAddr(event.params.recipient, factory);
 }
